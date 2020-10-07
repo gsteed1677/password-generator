54f29c9d12b8ad9bed37ea0e1cee6ed394f34e1e
Garrett Steed <steed.garrett@gmail.com>
2020-10-07 11:31:20 -0600

    trying different array/variable I found online to clean up and tighten the code

--- Develop/script.js
+++ Develop/script.js
@@ -12,79 +12,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//ARRAYS
-var lowerCase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = [
-  "!",
-  "%",
-  "&",
-  ",",
-  "*",
-  "+",
-  "-",
-  ".",
-  "/",
-  "<",
-  ">",
-  "?",
-  "~",
-];
+//ARRAYS OR VARIABLE STRINGS
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var specialChar = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
 //VARIABLES - to confirm with characteristics of password
 var confirmLength = "";
@@ -92,6 +23,7 @@ var confirmSpecialChar;
 var confirmNumericChar;
 var confirmUpperCase;
 var confirmLowerCase;
+var password = "";
 
 //FUNCTIONS FOR LENGTH
 
@@ -113,10 +45,12 @@ while (confirmLength <= 8 || confirmLength >= 128) {
 var confirmSpecialChar = confirm(
   "Click OK for special characters in your password"
 );
-var confirmNumericChar = confirm("Click OK to add a numbers to your password");
+var confirmNumericChar = confirm("Click OK to add numbers to your password");
 var confirmLowerCase = confirm(
   "Click OK for lower case letters in your password"
 );
 var confirmUpperCase = confirm(
   "Click OK for upper case letters in your password"
 );
+
+//PASSWORD CONFIRMS ASSIGNED VALUE
